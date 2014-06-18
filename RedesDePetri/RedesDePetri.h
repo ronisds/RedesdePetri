24b1645b60851c805ae334a7e0f039f5cdc72cb0
ronisds <ronizsds@gmail.com>
2014-06-18 15:58:58 -0300

    Parte da classe RedesDePetri!

--- RedesDePetri/RedesDePetri.h
+++ RedesDePetri/RedesDePetri.h
@@ -10,4 +10,46 @@
 
 @interface RedesDePetri : NSObject
 
+
+-(id) init;
+
+//Retorna NO se já existir um lugar com o mesmo nome
+-(BOOL)adicionarLugar:(NSString *) lugar;
+
+//Retorna NO se já existir uma transição com o mesmo nome
+-(BOOL)adicionarTransicao:(NSString *) transicao;
+
+//Se já existir, altera o peso da anterior e retorna YES
+//Retorna NO se não existir um lugar com o nome passado ou se não existir uma transição com o nome passado
+-(BOOL)adicionarArcoDeLugar:(NSString *) lugar paraTransicao:(NSString*) transicao comPeso:(NSInteger) peso;
+
+//Se já existir, altera o peso da anterior e retorna YES
+//Retorna NO se não existir um lugar com o nome passado ou se não existir uma transição com o nome passado
+-(BOOL)adicionarArcoDeTransicao:(NSString *) transicao paraLugar:(NSString*) lugar comPeso:(NSInteger) peso;
+
+//Retorna os lugares de entrada da transição
+//Retorna nil se a transição não existir
+-(NSArray*)entradasDeTransicao:(NSString *) transicao;
+
+//Retorna os lugares de saida da transicao
+//Retorna nil se a transição não existir
+-(NSArray*)saidasDeTransicao:(NSString *) transicao;
+
+//Recebe um dicionario onde as chaves sao os lugares e os valores sao as marcacoes
+//Reseta o historico de marcações da rede, tendo apenas a marcação recebida como marcação inicial
+//Retorna NO se o dicionário não possuir todos os lugares existentes na rede e apenas estes lugares, neste caso não reseta o histórico
+-(BOOL) setarMarcacao:(NSDictionary*) marcacao;
+
+//Retorna a marcação atual
+-(NSDictionary *) marcacaoAtual;
+
+//Retorna um array de marcações (Cada marcação é um NSDictionary)
+-(NSArray *) historicoDeMarcacoes;
+
+//Retorna as transições habilitadas
+-(NSArray *) transicoesHabilitadas;
+
+//Retorna NO se não existir uma transição com o nome passado ou se ela não estiver habilitada
+-(BOOL) aplicarTransicao:(NSString *) transicao;
+
 @end
--- RedesDePetri/RedesDePetri.m
+++ RedesDePetri/RedesDePetri.m
@@ -8,6 +8,178 @@
 
 #import "RedesDePetri.h"
 
+
+@interface Arco : NSObject
+
+@property (strong, nonatomic) NSString *lugar;
+@property (strong, nonatomic) NSString *transicao;
+@property (nonatomic) NSInteger peso;
+
+@end
+
+
+
+@interface RedesDePetri ()
+
+@property (strong, nonatomic) NSMutableSet *lugares;
+@property (strong, nonatomic) NSMutableSet *transicoes;
+@property (strong, nonatomic) NSMutableArray *arcosDeEntrada;
+@property (strong, nonatomic) NSMutableArray *arcosDeSaida;
+@property (strong, nonatomic) NSMutableDictionary *marcacao;
+@property (strong, nonatomic) NSMutableArray *historicoDeMarcacoes;
+
+@end
+
 @implementation RedesDePetri
+
+-(id)init {
+    if (self = [super init]) {
+        _lugares = [NSMutableSet new];
+        _transicoes = [NSMutableSet new];
+        _arcosDeEntrada = [NSMutableArray new];
+        _arcosDeSaida = [NSMutableArray new];
+        _historicoDeMarcacoes = [NSMutableArray new];
+        _marcacao = [NSMutableDictionary new];
+    }
+    return self;
+}
+
+-(BOOL)adicionarLugar:(NSString *)lugar {
+    if (![_lugares containsObject:lugar]) {
+        [_marcacao setValue:0 forKey:lugar];
+        [_lugares addObject:lugar];
+        return YES;
+    }
+    return NO;
+}
+
+-(BOOL) adicionarTransicao:(NSString *)transicao {
+    if (![_transicoes containsObject:transicao]) {
+        [_transicoes addObject:transicao];
+        return YES;
+    }
+    return NO;
+}
+
+-(BOOL) adicionarArcoDeLugar:(NSString *)lugar paraTransicao:(NSString *)transicao comPeso:(NSInteger)peso {
+    if (![_lugares containsObject:lugar] || ![_transicoes containsObject:transicao]) {
+        return NO;
+    }
+    
+    for (Arco *a in _arcosDeEntrada) {
+        if ([a.lugar isEqualToString:lugar] && [a.transicao isEqualToString:transicao]) {
+            [a setPeso:peso];
+            return YES;
+        }
+    }
+    
+    Arco *novoArco = [Arco new];
+    novoArco.lugar = lugar;
+    novoArco.transicao = transicao;
+    
+    [_arcosDeEntrada addObject:novoArco];
+    
+    return YES;
+}
+
+-(BOOL) adicionarArcoDeTransicao:(NSString *)transicao paraLugar:(NSString *)lugar comPeso:(NSInteger)peso {
+    if (![_lugares containsObject:lugar] || ![_transicoes containsObject:transicao]) {
+        return NO;
+    }
+    
+    for (Arco *a in _arcosDeSaida) {
+        if ([a.lugar isEqualToString:lugar] && [a.transicao isEqualToString:transicao]) {
+            [a setPeso:peso];
+            return YES;
+        }
+    }
+    
+    Arco *novoArco = [Arco new];
+    novoArco.lugar = lugar;
+    novoArco.transicao = transicao;
+    
+    [_arcosDeSaida addObject:novoArco];
+    
+    return YES;
+}
+
+-(NSArray*)entradasDeTransicao:(NSString *)transicao {
+    if (![_transicoes containsObject:transicao]) {
+        return nil;
+    }
+    NSMutableArray *retorno = [NSMutableArray new];
+    
+    for (Arco *a in _arcosDeEntrada) {
+        if ([a.transicao isEqualToString:transicao]) {
+            [retorno addObject:a.lugar];
+        }
+    }
+    
+    return retorno;
+}
+
+-(NSArray*)saidasDeTransicao:(NSString *)transicao {
+    if (![_transicoes containsObject:transicao]) {
+        return nil;
+    }
+    
+    NSMutableArray *retorno = [NSMutableArray new];
+    
+    for (Arco *a in _arcosDeSaida) {
+        if ([a.transicao isEqualToString:transicao]) {
+            [retorno addObject:a.lugar];
+        }
+    }
+    
+    return retorno;
+}
+
+-(BOOL) setarMarcacao:(NSDictionary *)marcacao {
+    NSArray *keys = [marcacao allKeys];
+    if ([keys count] != [_lugares count]) {
+        return NO;
+    }
+    for (NSString *lugar in _lugares) {
+        if (![keys containsObject:lugar]) {
+            return NO;
+        }
+    }
+    
+    _marcacao = [NSMutableDictionary dictionaryWithDictionary:marcacao];
+    [_historicoDeMarcacoes removeAllObjects];
+    [_historicoDeMarcacoes addObject:_marcacao];
+    
+    return YES;
+}
+
+-(NSDictionary *) marcacaoAtual {
+    return _marcacao;
+}
+
+-(NSArray *) historicoDeMarcacoes {
+    return _historicoDeMarcacoes;
+}
+
+
+// TO DO
+-(NSArray*) transicoesHabilitadas {
+    NSMutableArray *retorno = [NSMutableArray new];
+    
+    return retorno;
+}
+
+
+// TO DO
+-(BOOL) aplicarTransicao:(NSString *)transicao {
+    BOOL retorno = YES;
+    
+    if (![_transicoes containsObject:transicao]) {
+        return NO;
+    }
+    
+    
+    
+    return retorno;
+}
 
 @end
